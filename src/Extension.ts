e620ddfce77d8921bebf15e3c46f7366a1763352
Heitor Castro <heitorsales@gmail.com>
2020-10-16 22:35:40 -0500

    Get Explorer context, with target file path

--- src/Extension.ts
+++ src/Extension.ts
@@ -1,31 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) 
 {
 	// Display a message box to the user
-	vscode.window.showInformationMessage('Needle has been activated!');
+	//vscode.window.showInformationMessage('Needle has been activated!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('needle.inject', () => {
+	let disposable = vscode.commands.registerCommand('needle.inject', (e) => {
 		// The code you place here will be executed every time your command is executed
 
+		// Explorer context path
+		let path = e.fsPath;
+
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Needle!');
+		//vscode.window.showInformationMessage('Hello World from Needle!');
 
-		vscode.window.showQuickPick(["a", "b"]);
+		var fileTemplateFolder =  'C:/Users/heitor/Developer/.templates/files';
+				
+		const dirents = fs.readdirSync(fileTemplateFolder, { withFileTypes: true });
+		var filesNames = dirents.map(dirent => dirent.name);
 
-		vscode.workspace.openTextDocument('c:/Users/heitor/Desktop/test.txt').then((document) => {
-            let text = document.getText();
-		  });
+		 vscode.window.showQuickPick(filesNames).then(selection => 
+			{
+				vscode.window.showInformationMessage("Selected " + selection);
 
-		  var templateManager = new TemplateManager("a","b");
-		  
+				vscode.window.showInputBox().then(filename =>
+					{
+						var a = 1;
+					});
+		 	}
+		 );
 	});
 
 	context.subscriptions.push(disposable);
@@ -36,3 +47,4 @@ export function deactivate()
 {
 	vscode.window.showInformationMessage('Needle has been deactivated!');
 }
+
